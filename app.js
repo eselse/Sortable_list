b0755c49264ae7507cf2d644a82ecaff5408ea81
eselse <ilikewebdevelopment@gmail.com>
2021-06-18 20:48:41 +0400

    add markup & basic function for display list

---
+++ app.js
@@ -0,0 +1,45 @@
+// Define constants
+const draggableList = document.getElementById("draggable-list");
+const check = document.getElementById("check");
+
+const richestPeople = [
+  "Jeff Bezos",
+  "Bill Gates",
+  "Warren Buffet",
+  "Bernard Arnault",
+  "Carlos Slim Helu",
+  "Amancio Ortega",
+  "Larry Ellison",
+  "Mark Zukerberg",
+  "Michael Bloomberg",
+  "Larry Page",
+];
+
+// Store list items
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+// Functions =================================================================
+// Insert list items into DOM
+function createList() {
+  [...richestPeople].forEach((person, index) => {
+    const listItem = document.createElement("li");
+
+    listItem.setAttribute("data-index", index);
+
+    listItem.innerHTML = `
+        <span class="number">${index + 1}</span>
+        <div class="draggable" draggable="true">
+            <p class="person-name">${person}</p>
+            <i class="fa fa-grip-lines"></i>
+        </div>
+      `;
+
+    listItems.push(listItem);
+
+    draggableList.appendChild(listItem);
+  });
+}
